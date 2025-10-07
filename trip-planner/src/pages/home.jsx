3b0084e799fad585acd7b14f4ab96d4b973732c4
TauseefHaq <tauseefhaq777@gmail.com>
2025-10-07 22:16:22 +0530

    QOL changes

--- trip-planner/src/pages/home.jsx
+++ trip-planner/src/pages/home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/hero";
 import Navbar from "../components/navbar";
 import Destination from "../components/destination";
 import Trip from "../components/trip";
+import heroImg from "../assets/hero.jpg"
 import Footer from "../components/footer";
 
 function Home() {
@@ -10,7 +11,7 @@ function Home() {
       <Navbar></Navbar>
       <Hero
         cName="hero"
-        heroImg=".\src\assets\hero.jpg"
+        heroImg={heroImg}
         title="Make memories not travel plans"
         text="Book your travel plans today"
         btnText="Book travels"
